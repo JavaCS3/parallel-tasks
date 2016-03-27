db9dd02087ae9db8d0888fb68c9b594e86c8c7b3
charles <javacs3@gmail.com>
2016-03-27 14:13:30 +0800

    refactor codes

--- parallel-tasks.py
+++ parallel-tasks.py
@@ -1,42 +1,65 @@
 #!/usr/bin/env python
+# @author: Chalres Wei
+
 import multiprocessing, subprocess
 import shlex
-import os, sys
+import sys
 
 
 def task(cmd):
     cp = multiprocessing.current_process()
-    print 'Run task: %s (pid: %s)...' % (cmd, cp.pid)
+    task_name = 'task: %s (pid: %s)' % (cp.name, cp.pid)
+
+    print 'Run %s ...' % task_name
     
-    try:
-        cmd_process = subprocess.Popen(
-            shlex.split(cmd),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT
-        )
+    cmd_process = subprocess.Popen(
+        shlex.split(cmd),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
 
-        output, _ = cmd_process.communicate()
-        print '(pid: %s)\n' % cp.pid, output
+    output, _ = cmd_process.communicate()
 
-    except Exception, e:
-        raise
+    print '%s => \n%s' % (task_name, output)
+
+    sys.exit(cmd_process.returncode)
 
 
-if __name__=='__main__':
+def build_jobs(cmds):
     jobs = []
-
     for cmd in sys.argv[1:]:
         p = multiprocessing.Process(target=task, name=cmd, args=(cmd, ))
         p.daemon = True
         jobs.append(p)
+    return jobs
 
+
+def start_jobs(jobs):
     for j in jobs:
         j.start()
 
+
+def wait_jobs(jobs):
     for j in jobs:
         j.join()
-        print 'task: %s return: %s' % (j.name, j.exitcode)
 
 
+def get_jobs_results(jobs):
+    exitcode = 0
+    for j in jobs:
+        task_name = 'task: %s (pid: %s)' % (j.name, j.pid)
+        result = 'OK' if j.exitcode == 0 else 'FAILED'
 
- 
+        if j.exitcode != 0: exitcode = 255
+        print '%s [%s]' % (task_name, result)
+
+    return exitcode
+
+
+if __name__=='__main__':
+    jobs = build_jobs(sys.argv[1:])
+    start_jobs(jobs)
+    wait_jobs(jobs)
+
+    print '--- All tasks finished ---'
+    sys.exit(get_jobs_results(jobs))
